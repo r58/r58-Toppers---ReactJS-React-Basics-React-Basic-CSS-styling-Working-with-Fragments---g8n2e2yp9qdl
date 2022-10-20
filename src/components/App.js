aaa3d1b7410e5d61b3a5de953b581f2bf5ef646d
r58 <rohitkarande543@gmail.com>
2022-10-21 00:07:49 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,67 @@
+import React, {Component, useState} from "react";
+import "./../styles/App.css";
+
+class App extends Component {
+	constructor(props)
+	{
+		super(props);
+		this.state = {
+			list : [
+				{
+					name : "naman",
+					percent : 60.0000
+				},
+				{
+					name : "ritesh",
+					percent : 99.9431
+				},
+				{
+					name : "hitesh",
+					percent : 65.3365
+				},
+				{
+					name : "shreya",
+					percent : 80.6400
+				},
+				{
+					name : "ankur",
+					percent : 78.8970
+				},
+				{
+					name : "vanshu",
+					percent : 78.9876
+				},
+				{
+					name : "krupal",
+					percent : 85.3214
+				},
+			],
+		}
+	};
+    render() {
+    	return(
+    		<div id="main">
+    			{
+    				this.state.list.map(function(item,index){
+				   		if(item.percent > 75)
+				   		{
+				   			return(
+				   				<div key={index} className={(item.percent>=90)?("bg-pink"):("")}>
+					   				<div className="name">
+					   					{item.name} 
+					   				</div>
+					   				<div className="percent">
+					   					{item.percent.toFixed(2)} 
+					   				</div>
+				   				</div>
+				   				);
+				   		}
+			       })
+			    }
+    		</div>
+    		);
+    }
+}
+
+
+export default App;
